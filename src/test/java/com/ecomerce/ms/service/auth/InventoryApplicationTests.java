abfd59d861e3a5555955e3b4bbcf620821df9184
huyle <ghuy102@gmail.com>
2024-08-21 08:35:50 +0700

    Init auth service

---
+++ src/test/java/com/ecomerce/ms/service/auth/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomerce.ms.service.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
